9ce61c796db3d52b5b778edf86e099ba654c3879
Jaroslav Buday <jaroslav.buday@gmail.com>
2023-09-14 10:25:03 +0200

    script.js zmena farby h1 pomocou mouseleave a mouseenter

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ myCheckBox.addEventListener("change", function(event){
 let mySelect = document.querySelector("#my-select").addEventListener("change", function(event){
   console.log(event.target.value)
 })
+
+let heading = document.querySelector("h1")
+heading.addEventListener("mouseenter", function(event){
+  heading.style.color = "white"
+  heading.style.backgroundColor = "blue"
+})
+heading.addEventListener("mouseleave", function(event){
+  heading.style.color = "red"
+  heading.style.backgroundColor = "black"
+})
