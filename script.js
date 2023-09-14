28061e206e6a85d79917312c9b5bfdc30ed39587
Jaroslav Buday <jaroslav.buday@gmail.com>
2023-09-14 09:33:41 +0200

    script na vypis roletky do konzoly

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ let myCheckBox = document.querySelector("#my-check-box")
 myCheckBox.addEventListener("change", function(event){
   console.log(event.target.checked)
 })
+
+let mySelect = document.querySelector("#my-select").addEventListener("change", function(event){
+  console.log(event.target.value)
+})
